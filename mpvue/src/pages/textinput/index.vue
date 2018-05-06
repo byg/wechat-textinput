76462f5091b42505f65e5b685d04f25a4829b0b8
byg <byg@chinairi.com>
2018-05-06 15:59:32 +0800

    update README.md

--- mpvue/src/pages/textinput/index.vue
+++ mpvue/src/pages/textinput/index.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="container" @click="clickHandle('test click', $event)">
-    <form class="form-container">
-        <text-input name='default' type='text'  label='文本' />
-        <text-input name='username' type='text' placeholder='I am placeholder' label='用户名' />
-        <text-input name='text' type='text' placeholder='输入文字' label='文本' />
-        <text-input name='number' type='number' placeholder='只能输入数字' label='数字' />
-        <text-input name='email' type='email' placeholder='输入邮箱' label='邮箱' />
-        <text-input name='idCard' type='idCard' placeholder='输入身份证' label='身份证' />
-        <text-input name='digit' type='digit' placeholder='数字带小数点' label='小数' />
-        <text-input name='username' type='text' value='输入框已禁用' disabled='true' label='文本' />
-        <text-input name='username' type='text' maxLength='6' placeholder='最多只能输入6位' label='文本' />
-    </form>
+  <div class="container">
+      <text-input name='default' type='text'  label='文本' />
+      <text-input name='username' type='text' placeholder='I am placeholder' label='用户名' />
+      <text-input name='text' type='text' placeholder='输入文字' label='文本' />
+      <text-input name='number' type='number' placeholder='只能输入数字' label='数字' />
+      <text-input name='email' type='email' placeholder='输入邮箱' label='邮箱' />
+      <text-input name='idCard' type='idCard' placeholder='输入身份证' label='身份证' />
+      <text-input name='digit' type='digit' placeholder='数字带小数点' label='小数' />
+      <text-input name='username1' type='text' value='输入框已禁用' disabled='true' label='文本' />
+      <text-input name='username2' type='text' maxLength='6' placeholder='最多只能输入6位' label='文本' />
   </div>
 </template>
 
@@ -25,60 +23,31 @@ export default {
   },
 
   components: {
-    'text-input': textInput
-  },
-
-  methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      wx.navigateTo({ url })
-    },
-    getUserInfo () {
-      // 调用登录接口
-      wx.login({
-        success: () => {
-          wx.getUserInfo({
-            success: (res) => {
-              this.userInfo = res.userInfo
-            }
-          })
-        }
-      })
-    },
-    clickHandle (msg, ev) {
-      console.log('clickHandle:', msg, ev)
-    }
-  },
-
-  created () {
-    // 调用应用实例的方法获取全局数据
-    this.getUserInfo()
+    'text-input': textInput,
+    'text-input1': textInput,
+    'text-input2': textInput,
+    'text-input3': textInput,
+    'text-input4': textInput,
+    'text-input5': textInput,
+    'text-input6': textInput,
+    'text-input7': textInput,
+    'text-input8': textInput,
+    'text-input9': textInput,
+    'text-input10': textInput,
+    'text-input11': textInput,
+    'text-input12': textInput,
+    'text-input13': textInput,
+    'text-input14': textInput,
+    'text-input15': textInput,
+    'text-input16': textInput,
+    'text-input17': textInput,
+    'text-input18': textInput,
+    'text-input19': textInput
   }
 }
 </script>
 
 <style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
-}
-
-.userinfo-nickname {
-  color: #aaa;
-}
-
-.usermotto {
-  margin-top: 150px;
-}
-
 .form-control {
   display: block;
   padding: 0 12px;
